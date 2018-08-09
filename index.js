95f6106cc8ded9fbbf232fbac9d2947404d0a59e
Andy Burke <andy.burke@mailbox.earth>
2018-08-08 17:58:27 -0700

    Add support for schedulable tasks

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const path = require( 'path' );
 
 const api = require( 'fs-router' )( path.join( process.cwd(), 'api' ) );
 
+console.log( `NODE_ENV: ${ process.env.NODE_ENV }` );
+
+// INIT
 ( async function() {
     const init_filename = path.join( process.cwd(), 'init.js' );
 
@@ -22,6 +25,44 @@ const api = require( 'fs-router' )( path.join( process.cwd(), 'api' ) );
     else if ( typeof init.init === 'function' ) {
         await init.init();
     }
+} )();
+
+// TASKS
+( async function() {
+    const tasks_dir = path.join( process.cwd(), 'tasks' );
+
+    if ( !fs.existsSync( tasks_dir ) ) {
+        return;
+    }
+
+    const require_dir = require( 'require-dir' );
+    const tasks = require_dir( tasks_dir );
+    
+    async function execute_task( task, task_name = 'unknown' ) {
+        console.log( `TASKS: Executing: ${ task.name || task_name }` );
+        await task.process();
+
+        if ( task.FREQUENCY ) {
+            setTimeout( execute_task.bind( null, task, task_name ), task.FREQUENCY );
+        }
+    }
+    
+    Object.keys( tasks ).forEach( task_name => {
+        const task = tasks[ task_name ];
+
+        if ( task.FREQUENCY ) {
+            console.log( `TASKS: Scheduling: ${ task.name || task_name }` );
+            setTimeout( async () => {
+                await execute_task( task, task_name );
+            }, task.INITIAL ? 0 : task.FREQUENCY );
+        }
+        else {
+            // just execute if there's no frequency
+            setTimeout( async () => {
+                await execute_task( task, task_name );
+            }, 0 );
+        }
+    } );
 } )();
 
 module.exports = async function( request, response ) {
