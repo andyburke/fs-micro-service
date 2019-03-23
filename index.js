707cefd328fd3327baa7a55c994338e71262ea62
Andy Burke <andy.burke@mailbox.earth>
2019-03-22 17:14:32 -0700

    allow passing API in

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const httpstatuses = require( 'httpstatuses' );
 const micro = require( 'micro' );
 const path = require( 'path' );
 
-const api = require( 'fs-router' )( path.join( process.cwd(), 'api' ) );
-
 console.log( `NODE_ENV: ${ process.env.NODE_ENV }` );
 
 // INIT
@@ -69,8 +67,13 @@ console.log( `NODE_ENV: ${ process.env.NODE_ENV }` );
 
 module.exports = function( _options ) {
     const options = extend( true, {
-        log: true
+        log: true,
+        api: null
     }, _options );
+
+    if ( !options.api ) {
+        options.api = require( 'fs-router' )( path.join( process.cwd(), 'api' ) );
+    }
 
     return async function( request, response ) {
 
@@ -78,7 +81,7 @@ module.exports = function( _options ) {
             console.log( `${ new Date().toISOString() } ${ get_request_ip( request ) } HTTP/${ request.httpVersion } HTTP${ request.connection && request.connection.encrypted ? 'S' : '' } ${ request.method } ${ request.url }` );
         }
 
-        const handler = api( request );
+        const handler = options.api( request );
 
         if ( !handler ) {
             micro.send( response, httpstatuses.not_found, {
